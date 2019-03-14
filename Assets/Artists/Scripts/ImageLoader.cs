cae4744085b1747319d90b61b85171f6f15fd8f4
h-nishihata <nishihata0301@gmail.com>
2019-03-14 13:02:41 +0900

    fix

--- Assets/Artists/Scripts/ImageLoader.cs
+++ Assets/Artists/Scripts/ImageLoader.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class ImageLoader : MonoBehaviour
 {
     private Rect sourceImageSize = new Rect(0, 0, 1920, 1200);
+    private Rect sourceThumbnailSize = new Rect(0, 0, 380, 380);
     private int numArtists;
     private int id;
     public string[] imageURLs;
-    private int numDone;
+    public string[] thumbnailURLs;
+    private int doneLoadImg;
+    private int doneLoadThumbs;
 
     public Text loadingText;
     public SceneLoader sceneLoader;
@@ -21,7 +24,13 @@ public class ImageLoader : MonoBehaviour
 
         foreach (var item in FetchedImages.Instance.images)
         {
-            StartCoroutine("GetTexture", id);
+            StartCoroutine("LoadImages", id);
+            id++;
+        }
+        foreach (var item in FetchedImages.Instance.thumbnails)
+        {
+            id = 0;
+            StartCoroutine("LoadThumbnails", id);
             id++;
         }
     }
@@ -30,14 +39,14 @@ public class ImageLoader : MonoBehaviour
     {
         loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
 
-        if (numDone >= numArtists * 2)
+        if ((doneLoadImg >= numArtists) && (doneLoadThumbs >= numArtists))
             sceneLoader.LoadMainScene();
     }
 
     /// <summary>
     /// 各ページの画像をロードする.
     /// </summary>
-    IEnumerator GetTexture(int pageID)
+    IEnumerator LoadImages(int pageID)
     {
         UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageURLs[id]);
         yield return www.SendWebRequest();
@@ -51,7 +60,24 @@ public class ImageLoader : MonoBehaviour
             Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
             FetchedImages.Instance.images[pageID] = Sprite.Create(myTexture, sourceImageSize, Vector2.zero);
         }
-        numDone++;
+        doneLoadImg++;
+    }
+
+    IEnumerator LoadThumbnails(int pageID)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(thumbnailURLs[id]);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            FetchedImages.Instance.thumbnails[pageID] = Sprite.Create(myTexture, sourceThumbnailSize, Vector2.zero);
+        }
+        doneLoadThumbs++;
     }
 }
 
